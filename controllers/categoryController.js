034de95daa15c650e354212defa34456650f3483
Flora2020 <rossignols1989@gmail.com>
2021-03-31 17:16:15 +0800

    Add feat: admin can edit a category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,15 +2,21 @@ const db = require('../models')
 const Category = db.Category
 
 const categoryController = {
-  getCategories: (req, res) => {
-    return Category.findAll({ raw: true })
-      .then(categories => {
-        return res.render('admin/categories', { categories })
-      })
-      .catch(error => {
-        console.log(error)
-        return res.render('error')
-      })
+  getCategories: async (req, res) => {
+    try {
+      const id = req.params.id
+      let category
+
+      if (id) {
+        category = (await Category.findByPk(id)).toJSON()
+      }
+
+      const categories = await Category.findAll({ raw: true })
+      return res.render('admin/categories', { categories, category })
+    } catch (error) {
+      console.log(error)
+      return res.render('error')
+    }
   },
 
   postCategory: (req, res) => {
@@ -27,6 +33,31 @@ const categoryController = {
       .catch(error => {
         console.log(error)
         return res.render('error')
+      })
+  },
+
+  putCategory: (req, res) => {
+    const id = req.params.id
+    const name = req.body.name
+    if (!name) {
+      req.flash('error_messages', '請輸入分類名稱！')
+      return res.redirect('back')
+    }
+
+    return Category.findByPk(id)
+      .then(category => {
+        if (!category) {
+          req.flash('error_messages', '查無此分類！')
+          return res.redirect('back')
+        }
+        category.update({ name })
+          .then(() => {
+            return res.redirect('/admin/categories')
+          })
+          .catch(error => {
+            console.log(error)
+            return res.render('error')
+          })
       })
   }
 }
