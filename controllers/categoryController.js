cfa1653ebe752434bdb46388b233d4f67c9e1c71
Flora2020 <rossignols1989@gmail.com>
2021-03-31 16:21:41 +0800

    Add feat: admin can create a category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -11,6 +11,23 @@ const categoryController = {
         console.log(error)
         return res.render('error')
       })
+  },
+
+  postCategory: (req, res) => {
+    const name = req.body.name
+    if (!name) {
+      req.flash('error_messages', '請輸入分類名稱！')
+      return res.redirect('back')
+    }
+
+    return Category.create({ name })
+      .then(() => {
+        return res.redirect('/admin/categories')
+      })
+      .catch(error => {
+        console.log(error)
+        return res.render('error')
+      })
   }
 }
 
