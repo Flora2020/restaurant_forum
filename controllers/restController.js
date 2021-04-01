ee6f35ebfd395baa8d6387394a858dfa05a144c8
Flora2020 <rossignols1989@gmail.com>
2021-04-01 10:44:12 +0800

    Add feat: user can read all restaurants

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,22 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 const restController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    Restaurant.findAll({ raw: true, nest: true, include: [Category] })
+      .then(restaurants => {
+        const data = restaurants.map((restaurant) => ({
+          ...restaurant,
+          description: restaurant.description.substring(0, 50),
+          categoryName: restaurant.Category.name
+        }))
+        return res.render('restaurants', { restaurants: data })
+      })
+      .catch(error => {
+        console.log(error)
+        return res.render('error')
+      })
   }
 }
 module.exports = restController
