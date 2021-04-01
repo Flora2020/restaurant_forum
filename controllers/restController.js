f6f139736801658f8ad17a385c148fbaa854a046
Flora2020 <rossignols1989@gmail.com>
2021-04-01 16:20:53 +0800

    Prevent showing too many restaurants in one page

--- controllers/restController.js
+++ controllers/restController.js
@@ -5,25 +5,47 @@ const Category = db.Category
 const restController = {
   getRestaurants: async (req, res) => {
     try {
+      const pageLimit = 12
+      const page = Number(req.query.page) || 1
+      const offset = (page - 1) * pageLimit
       const whereQuery = {}
       let categoryId = ''
+
       if (req.query.categoryId) {
         categoryId = Number(req.query.categoryId)
         whereQuery.CategoryId = categoryId
       }
-      const restaurants = await Restaurant.findAll({
+
+      const restaurants = await Restaurant.findAndCountAll({
         raw: true,
         nest: true,
         include: [Category],
-        where: whereQuery
+        where: whereQuery,
+        limit: pageLimit,
+        offset
       })
       const categories = await Category.findAll({ raw: true, nest: true })
-      const data = restaurants.map((restaurant) => ({
+      const data = restaurants.rows.map((restaurant) => ({
         ...restaurant,
         description: restaurant.description.substring(0, 50),
         categoryName: restaurant.Category.name
       }))
-      return res.render('restaurants', { restaurants: data, categories, categoryId })
+
+      const pages = Math.ceil(restaurants.count / pageLimit)
+      const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+      const prevPage = page - 1 < 1 ? 1 : page - 1
+      const nextPage = page + 1 > pages ? pages : pages + 1
+
+      return res.render('restaurants', {
+        restaurants: data,
+        categories,
+        categoryId,
+        page,
+        pages,
+        totalPage,
+        prev: prevPage,
+        next: nextPage
+      })
     } catch (error) {
       console.log(error)
       return res.render('error')
