2af311d8931336339a58584e55bae95468adc889
Flora2020 <rossignols1989@gmail.com>
2021-04-09 19:47:12 +0800

    Improve page layout of  topTenRestaurant

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.engine('handlebars', handlebars({
 }))
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
